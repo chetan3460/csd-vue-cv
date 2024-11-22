80c34ea923bf73e6bedc18779bfc6cad09b457a9
chetan dhargalkar <chetan.dmc@gmail.com>
2024-11-22 09:31:27 +0530

    journey name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,10 +10,9 @@
 <script setup>
 import Banner from '@/components/banner.vue';
 import About from '@/components/About.vue';
-import Journey from '@/components/journey.vue';
+import Journey from '@/components/Journey.vue';
 import Skills from '@/components/Skills.vue'
 
 import { scrollSmooth } from '@/js/utilits'
-import noiseImage from "../assets/svg/noise.gif"
 scrollSmooth()
 </script>
